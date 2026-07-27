9939eb71f6a231a89351577ce2a9d27bac3a095a
Mariia Cheprakova <mariia.cheprakova@visma.com>
2026-07-27 15:31:54 +0300

    adjustments

--- Payments/Factories/PaymentStrategyFactory.cs
+++ Payments/Factories/PaymentStrategyFactory.cs
@@ -1,10 +1,5 @@
 ﻿using SolidOrderProcessor.Logging;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using SolidOrderProcessor.Logging;
 using SolidOrderProcessor.Models;
-using System.Security.Cryptography.X509Certificates;
 
 namespace SolidOrderProcessor.Payments;
 
@@ -12,16 +7,15 @@ public class PaymentStrategyFactory : IPaymentStrategyFactory
 {
     private readonly ILogger _logger;
     public PaymentStrategyFactory(ILogger logger) => _logger = logger;
-    
+
     public IPaymentStrategy Create(PaymentMethod paymentMethod)
     {
         return paymentMethod switch
         { PaymentMethod.CreditCard => new CreditCardPayment(_logger),
-          PaymentMethod.PayPal => new PayPalPayment(_logger),
-          PaymentMethod.BankTransfer => new BankTransferPayment(_logger),
-          _ => throw new ArgumentException("Unsupported payment method.") 
+            PaymentMethod.PayPal => new PayPalPayment(_logger),
+            PaymentMethod.BankTransfer => new BankTransferPayment(_logger),
+            _ => throw new ArgumentOutOfRangeException("Unsupported payment method.")
 
         };
     }
-
 }
