be7249014df7426aeaa73447ac31364beee5d675
MariiaChepra <mariiachepra@gmail.com>
2026-07-27 04:46:58 +0300

    paymrnt strategy engineering

---
+++ Payments/IPaymentStrategyFactory.cs
@@ -0,0 +1,11 @@
+﻿using SolidOrderProcessor.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SolidOrderProcessor.Payments;
+
+public interface IPaymentStrategyFactory
+{
+    IPaymentStrategy Create(PaymentMethod paymentMethod);
+}
---
+++ Payments/PaymentStrategyFactory.cs
@@ -0,0 +1,46 @@
+﻿using SolidOrderProcessor.Configuration;
+using SolidOrderProcessor.Logging;
+using SolidOrderProcessor.Models;
+
+namespace SolidOrderProcessor.Payments;
+
+public class PaymentStrategyFactory : IPaymentStrategyFactory
+{
+    private readonly ILogger _logger;
+
+    public PaymentStrategyFactory(ILogger logger)
+    {
+        _logger = logger;
+    }
+
+    public IPaymentStrategy Create(PaymentMethod paymentMethod)
+    {
+        IPaymentStrategy strategy = paymentMethod switch
+        {
+            PaymentMethod.CreditCard =>
+                new CreditCardPayment(_logger),
+
+            PaymentMethod.PayPal =>
+                new PayPalPayment(_logger),
+
+            PaymentMethod.BankTransfer =>
+                new BankTransferPayment(_logger),
+
+            _ => throw new ArgumentException(
+                "Unsupported payment method.")
+        };
+
+        if (AppSettings.Instance.EnablePaymentLogging)
+        {
+            strategy = new PaymentLoggingDecorator(
+                strategy,
+                _logger);
+        }
+
+        strategy = new PaymentTimingDecorator(
+            _logger,
+            strategy);
+
+        return strategy;
+    }
+}
